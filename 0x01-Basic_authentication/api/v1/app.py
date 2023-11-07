3fb1e95bd7970be82ec8cab22701ca7b7d074459
georgeeset <georgeperfect4u@gmail.com>
2023-11-07 19:16:17 +0100

    fix for task 1 with error handler 401 function at app.py

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -27,8 +27,15 @@ if __name__ == "__main__":
     app.run(host=host, port=port)
 
 
-@app.errorhandler(404)
-def not_found(error) -> str:
-    """ Not found handler
+@app.errorhandler(401)
+def unauthorized(error) -> str:
+    """ Unauthorized handler
     """
-    return jsonify({"error": "Not found"}), 404
+    return jsonify({"error": "Unauthorized"}), 401
+
+
+@app.errorhandler(403)
+def forbidden(error) -> str:
+    """ Forbidden handler
+    """
+    return jsonify({"error": "Forbidden"}), 403
