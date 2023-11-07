341084841eb2d13274b4c778524188b32eb9ad20
georgeeset <georgeperfect4u@gmail.com>
2023-11-07 19:06:15 +0100

    401 unauthorized error implemented

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -25,3 +25,10 @@ if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
     port = getenv("API_PORT", "5000")
     app.run(host=host, port=port)
+
+
+@app.errorhandler(404)
+def not_found(error) -> str:
+    """ Not found handler
+    """
+    return jsonify({"error": "Not found"}), 404
