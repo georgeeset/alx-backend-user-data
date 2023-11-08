574e92a6f6295d5bf009c43f0c85feafbe5bef2e
georgeeset <georgeperfect4u@gmail.com>
2023-11-08 11:05:27 +0100

    require_auth method completed

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -13,7 +13,14 @@ class Auth:
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """ will be used later, now, you dont
         need to take care of them """
-        pass
+        if path is None or not excluded_paths:
+            return True
+        for i in excluded_paths:
+            if i.endswith('*') and path.startswith(i[:-1]):
+                return False
+            elif i in {path, path + '/'}:
+                return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         """ that returns None - request """
