85246f0492c5a7f69eb7cc0614a8acdd7792ccd9
georgeeset <georgeperfect4u@gmail.com>
2023-11-08 15:51:06 +0100

    base64 auth method implemented

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -9,4 +9,12 @@ from api.v1.auth.auth import Auth
 class BasicAuth(Auth):
     """ Basic Authentication """
 
-    pass
+    def extract_base64_authorization_header(
+                self, authorization_header: str) -> str:
+            """ returns the Base64 part of the
+            Authorization header for a Basic Authentication:
+            """
+            if authorization_header and isinstance(
+                    authorization_header,
+                    str) and authorization_header.startswith("Basic "):
+                return authorization_header[6:]
