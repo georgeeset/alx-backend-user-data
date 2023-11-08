79b5c3b6c5efff7c023f590774cd1a036b32702c
georgeeset <georgeperfect4u@gmail.com>
2023-11-08 15:56:53 +0100

    pycodestyle test passed

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -10,11 +10,12 @@ class BasicAuth(Auth):
     """ Basic Authentication """
 
     def extract_base64_authorization_header(
-                self, authorization_header: str) -> str:
-            """ returns the Base64 part of the
-            Authorization header for a Basic Authentication:
-            """
-            if authorization_header and isinstance(
-                    authorization_header,
-                    str) and authorization_header.startswith("Basic "):
-                return authorization_header[6:]
+            self, authorization_header: str
+            ) -> str:
+        """ returns the Base64 part of the
+        Authorization header for a Basic Authentication:
+        """
+        if authorization_header and isinstance(
+                authorization_header,
+                str) and authorization_header.startswith("Basic "):
+            return authorization_header[6:]
