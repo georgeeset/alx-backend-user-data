0d1ef0c26e32b4db28539903c76fd31a92e80c67
georgeeset <georgeperfect4u@gmail.com>
2023-11-09 10:06:51 +0100

    session auth mech

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -3,7 +3,7 @@
 Route module for the API
 """
 from os import getenv
-from api.v1.auth import auth
+from api.v1.auth import auth, session_auth
 from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
@@ -15,6 +15,13 @@ app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
 auth = None
+auth_type = getenv('AUTH_TYPE', 'auth')
+if auth_type == 'auth':
+    auth = Auth()
+if auth_type == 'basic_auth':
+    auth = BasicAuth()
+if auth_type == 'session_auth':
+    auth = session_auth()
 
 if getenv("AUTH_TYPE") == 'basic_auth':
     from api.v1.auth.basic_auth import BasicAuth
