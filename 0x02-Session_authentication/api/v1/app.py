cc0754c8043eca33581c0a02d057996152c91e3d
georgeeset <georgeperfect4u@gmail.com>
2023-11-09 09:49:02 +0100

    user login with me

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -55,17 +55,17 @@ def forbidden(error) -> str:
 def request_filter() -> None:
     """ Checks if request needs authorization
     """
-    if auth:
-        excluded_paths = [
+    excluded_paths = [
         '/api/v1/status/',
         '/api/v1/unauthorized/',
-        '/api/v1/forbidden/'
-        "/api/v1/auth_session/login/",
+        '/api/v1/forbidden/',
+        '/api/v1/auth_session/login/'
         ]
-        if auth.require_auth(request.path, excluded_paths):
-            user = auth.current_user(request)
-            if auth.authorization_header(request) is None:
-                abort(401)
-            if user is None:
-                abort(403)
-            request.current_user = user
+
+    if auth and auth.require_auth(request.path, excluded_paths):
+        if auth.authorization_header(request) is None and auth.session_cookie(
+                request) is None:
+            abort(401)
+        if auth.current_user(request) is None:
+            abort(403)
+        request.current_user = auth.current_user(request)
