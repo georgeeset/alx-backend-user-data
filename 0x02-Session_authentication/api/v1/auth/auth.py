996a9fde4dedd14eff7ef9fbf375454314e288ba
georgeeset <georgeperfect4u@gmail.com>
2023-11-09 09:39:56 +0100

    directory name fixed

---
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""
+API authentication module
+"""
+
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    """ Authentication """
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """ will be used later, now, you dont
+        need to take care of them """
+        if path is None or not excluded_paths:
+            return True
+        for i in excluded_paths:
+            if i.endswith('*') and path.startswith(i[:-1]):
+                return False
+            elif i in {path, path + '/'}:
+                return False
+        return True
+
+    def authorization_header(self, request=None) -> str:
+        """ Checks if Authorization request header is present in header
+        """
+        if request is None or "Authorization" not in request.headers:
+            return None
+        else:
+            return request.headers.get('Authorization')
+
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """ Flask request object"""
+        return None
