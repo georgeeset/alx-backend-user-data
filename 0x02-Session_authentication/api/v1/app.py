d1818cf31cc8e99257bac8604bb149af6614c1a2
georgeeset <georgeperfect4u@gmail.com>
2023-11-09 10:44:42 +0100

    relocated fiel format

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -7,7 +7,6 @@ from api.v1.auth import auth, session_auth
 from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
-import os
 
 
 app = Flask(__name__)
