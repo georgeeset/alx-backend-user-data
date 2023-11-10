7d9c5a9c20e8285b58c53314f279c6f301939ee5
georgeeset <georgeperfect4u@gmail.com>
2023-11-10 14:58:34 +0100

    restore old setup

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -3,10 +3,11 @@
 Route module for the API
 """
 from os import getenv
-from api.v1.auth import auth, session_auth
+from api.v1.auth import auth
 from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
+import os
 
 
 app = Flask(__name__)
@@ -14,13 +15,6 @@ app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
 auth = None
-auth_type = getenv('AUTH_TYPE', 'auth')
-if auth_type == 'auth':
-    auth = Auth()
-if auth_type == 'basic_auth':
-    auth = BasicAuth()
-if auth_type == 'session_auth':
-    auth = session_auth()
 
 if getenv("AUTH_TYPE") == 'basic_auth':
     from api.v1.auth.basic_auth import BasicAuth
@@ -57,21 +51,21 @@ def forbidden(error) -> str:
     """
     return jsonify({"error": "Forbidden"}), 403
 
-
 @app.before_request
 def request_filter() -> None:
     """ Checks if request needs authorization
     """
-    excluded_paths = [
+    if auth:
+        excluded_paths = [
         '/api/v1/status/',
         '/api/v1/unauthorized/',
-        '/api/v1/forbidden/',
-        '/api/v1/auth_session/login/'
+        '/api/v1/forbidden/'
+        "/api/v1/auth_session/login/",
         ]
-
-    if auth and auth.require_auth(request.path, excluded_paths):
-        if auth.authorization_header(request) is None:
-            abort(401)
-        if auth.current_user(request) is None:
-            abort(403)
-        request.current_user = auth.current_user(request)
+        if auth.require_auth(request.path, excluded_paths):
+            user = auth.current_user(request)
+            if auth.authorization_header(request) is None:
+                abort(401)
+            if user is None:
+                abort(403)
+            request.current_user = user
--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-""" Module of Session Authentication
-"""
-from api.v1.auth.auth import Auth
-
-
-class SessionAuth(Auth):
-    pass
