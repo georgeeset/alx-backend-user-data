dd7ee58dad3fcdb0dfcbadf0fa382de3f114296d
georgeeset <georgeperfect4u@gmail.com>
2023-11-11 11:15:21 +0100

    user_id_for_session_id method for session id verification

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -21,3 +21,14 @@ class SessionAuth(Auth):
             session_id = str(uuid4())
             self.user_id_by_session_id[session_id] = user_id
             return session_id
+        return None
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """Retrieves the user id associated with
+        given session id.
+        """
+        if session_id is None:
+            return None
+        if type(session_id) is str:
+            return self.user_id_by_session_id.get(session_id)
+        return None
