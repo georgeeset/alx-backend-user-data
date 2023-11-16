1d78addc14bf9422763ff16d9ffa946a6ab49a3d
georgeeset <georgeperfect4u@gmail.com>
2023-11-16 12:39:10 +0100

    generate uuid in auth module

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -5,6 +5,7 @@ import bcrypt
 from db import DB
 from sqlalchemy.orm.exc import NoResultFound
 from user import User
+from uuid import uuid4
 
 
 def _hash_password(password: str) -> bytes:
@@ -44,3 +45,8 @@ class Auth:
         except NoResultFound:
             return False
         return False
+
+    def _generate_uuid() -> str:
+        """Generates a UUID.
+        """
+        return str(uuid4())
